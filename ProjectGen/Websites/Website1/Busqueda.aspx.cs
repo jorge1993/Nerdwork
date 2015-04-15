ca55813df2d5b79b515841a2f8863e7433273fef
Jorge Vergara <jorgevergara1993@gmail.com>
2015-04-15 19:52:03 +0200

    acalkfjd

--- ProjectGen/Websites/Website1/Busqueda.aspx.cs
+++ ProjectGen/Websites/Website1/Busqueda.aspx.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:ProjectGen/Websites/Website1/Busqueda.aspx.cs
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,20 +14,4 @@ public partial class _Default : System.Web.UI.Page
         p_hobby = Request.QueryString["Hobby"];
         HyperLink1.Text = p_hobby;
     }
-
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-public partial class _Default : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
->>>>>>> origin/master:ProjectGen/Websites/Website1/Default.aspx.cs
 }
