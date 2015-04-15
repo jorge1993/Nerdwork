1a5167397b4f511dbf4cb35b691fe5b67d28e29d
CARLOS MARTINEZ RUEDA <cmr66@alu.ua.es>
2015-04-15 17:22:00 +0200

    mixed

--- ProjectGen/Websites/Website1/MasterPage2.master.cs
+++ ProjectGen/Websites/Website1/MasterPage2.master.cs
@@ -16,7 +16,7 @@ public partial class MasterPage2 : System.Web.UI.MasterPage
 
         if (Session.Count == 0)
         {
-            Response.Redirect("Default.aspx");
+            Response.Redirect("First.aspx");
         }
 
 
