8a402765bb20847bb534c0aa29e0f241ec741822
Jorge Vergara <jorgevergara1993@gmail.com>
2015-04-15 14:09:38 +0200

    inicio del post

--- ProjectGen/Websites/Website1/Default4.aspx
+++ ProjectGen/Websites/Website1/Default4.aspx
@@ -3,5 +3,13 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <p>
+        Write a post:</p>
+    <p>
+        <asp:TextBox ID="TextBox2" runat="server" Height="77px" MaxLength="150" TextMode="MultiLine" Width="100%"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Post" />
+    </p>
 </asp:Content>
 
--- ProjectGen/Websites/Website1/Default4.aspx.cs
+++ ProjectGen/Websites/Website1/Default4.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ProjectGenNHibernate.CEN.Project;
 
 public partial class Default4 : System.Web.UI.Page
 {
@@ -12,5 +13,12 @@ public partial class Default4 : System.Web.UI.Page
        
         
         
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        PostCEN post = new PostCEN();
+        //post.Create(0,TextBox2.Text, (String) Session["Name"]);
+        post.Create(  ,TextBox2.Text, (String)Session["Name"]);
+
     }
 }
