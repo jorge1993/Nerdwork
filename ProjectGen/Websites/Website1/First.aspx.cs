612010ad58d050ece6b750facede6e3e7312b6ba
Jorge Vergara <jorgevergara1993@gmail.com>
2015-04-18 12:24:04 +0200

    Hecho

--- ProjectGen/Websites/Website1/First.aspx.cs
+++ ProjectGen/Websites/Website1/First.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -19,15 +22,41 @@ public partial class _Default : System.Web.UI.Page
     {
         String nick = TextBox2.Text;
         String pass = TextBox7.Text;
+        IList<HobbyEN> lista;
+        int RowCount = 0;
 
         UsuarioCEN user = new UsuarioCEN();
         if (user.Searchbynick(nick) != null)
         {
             UsuarioEN en = user.Searchbynick(nick);
+
             if (en.Password == pass)
             {
                 Session["Name"] = nick;
-                if (en.Hobby==null)
+
+                String con = ConfigurationManager.ConnectionStrings["ProjectGenNHibernateConnectionString"].ToString();
+                SqlConnection sqlConnection1 = new SqlConnection(con);
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+
+                cmd.CommandText = "SELECT * FROM  hobby_user WHERE (FK_nickname_idUser = '" + nick + "')";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = sqlConnection1;
+
+                sqlConnection1.Open();
+
+                reader = cmd.ExecuteReader();
+                // Data is accessible through the DataReader object here.
+
+                while (reader.HasRows && reader.Read())
+                {
+                    RowCount++;
+                }
+                sqlConnection1.Close();
+
+
+
+                if (RowCount == 0)
                 {
                     Response.Redirect("ModifyProfile.aspx");
                 }
@@ -36,24 +65,28 @@ public partial class _Default : System.Web.UI.Page
                     Response.Redirect("PerfilPrivate.aspx");
                 }
             }
-       }
-        
-            Label10.Text = "Invalid nickname/password";
-            Label10.Visible = true;
         }
-    
+
+        Label10.Text = "Invalid nickname/password";
+        Label10.Visible = true;
+    }
+
 
     protected void Button2_Click(object sender, EventArgs e)
     {
         String email = TextBox5.Text;
         String nick = TextBox3.Text;
         String pass = TextBox4.Text;
-        System.Collections.Generic.IList<string> p_hobby = null;
+
+        //Collection<HobbyEN> p_hobby;
+        IList<HobbyEN> p_hobby = null;
+
+
 
         if (pass.Length >= 4 && pass.Length <= 16)
         {
             UsuarioCEN user = new UsuarioCEN();
-            user.Create(nick, email, pass, "", "", "", "~/images/default_avatar.png", p_hobby);
+            user.Create(nick, email, pass, "", "", "", "~/images/default_avatar.png", null);
             Label5.Text = "Register successfully";
             Label5.Visible = true;
         }
