08edfa283263aa1dba3a886b6edb97ef636f3354
CARLOS MARTINEZ RUEDA <cmr66@alu.ua.es>
2015-05-07 15:43:58 +0200

    Cosas nazis

---
+++ ProjectGen/Websites/Website1/Send.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage2.master" AutoEventWireup="true" CodeFile="Send.aspx.cs" Inherits="Send" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+<div style="float: left; margin-left:5%; width:20%">
+<p>
+<asp:Label id="destino" runat="server">ADDRESSEE</asp:Label>
+</p>
+<p>
+<asp:TextBox ID="reciever" ReadOnly="true" Height="30%" Width="50%" BorderColor="Black" runat="server">
+</asp:TextBox>
+</p>
+<p>
+<asp:ListBox ID="sendlist" runat="server" BorderColor="Black" ReadOnly="true" 
+        Height="20%" Width="45%" DataSourceID="SqlDataSourceMessages" DataTextField="nickname" DataValueField="nickname">
+            </asp:ListBox>  
+  </p>
+  <p>
+  <asp:Button ID="select1" runat="server" BorderColor="#999999" 
+                         BorderStyle="Solid" Text="SELECT" OnClick="Button_SelectSend"
+                          />
+  </p>
+   
+</div>
+
+<div style="float: right; margin-right: 5%; height:50%; width:70% ";>
+SUBJECT <asp:TextBox ID="subject" runat="server" BorderColor="Black" Height="8%" Width="80%"></asp:TextBox>
+
+<p style="float: right; margin-right: 5%; height:50%; width:100% ";>
+
+<asp:TextBox ID="writebox" runat="server" BorderColor="Black" Height="100%" Width="100%" TextMode="MultiLine"></asp:TextBox>
+</p>
+ 
+
+<p style="float: right; margin-right: 15%";>
+                          <asp:Label id="Label1" runat="server"></asp:Label>
+
+<asp:Button ID="Button" runat="server" BorderColor="#999999" 
+                         BorderStyle="Solid" Text="SEND" OnClick="Button_Send"
+                          />
+</p>
+</div>
+
+</asp:Content>
+
---
+++ ProjectGen/Websites/Website1/Send.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Data;
+using ProjectGenNHibernate.CEN.Project;
+using ProjectGenNHibernate;
+using ProjectGenNHibernate.CAD.Project;
+using System.Collections.Generic;
+
+public partial class Send : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void Button_SelectSend(object sender, EventArgs e)
+    {
+        int i;
+        for (i = 0; i < sendlist.Items.Count; i++)
+        {
+            ListItem item = sendlist.Items[i];
+            if (item.Selected == true)
+            {
+                reciever.Text = item.Text;
+                sendlist.Items.Remove(item);
+                break;
+            }
+        }
+    }
+
+    protected void Button_Send(object sender, EventArgs e)
+    {
+        int ID;
+        String s = subject.Text;
+        String d = writebox.Text;
+        String r = reciever.Text;
+        String n = Session["NAME"].ToString();
+
+        if (subject.Text == "" || subject.Text == null)
+        {
+            Label1.Text = "Subject is needed";
+            Label1.Visible = true;
+        }
+        else if (writebox.Text == "" || writebox.Text == null)
+        {
+            Label1.Text = "A message is needed";
+            Label1.Visible = true;
+        }
+
+        else if (reciever.Text == "" || reciever.Text == null)
+        {
+            Label1.Text = "A receiver is needed";
+            Label1.Visible = true;
+        }
+        else
+        {
+            MessagesCEN mes = new MessagesCEN();
+
+            String stmt1 = "SELECT MAX(ID) from Messages";
+            String con = ConfigurationManager.ConnectionStrings["ProjectGenNHibernateConnectionString"].ToString();
+
+            SqlConnection thisConnection = new SqlConnection(con);
+            SqlCommand cmdSelect = new SqlCommand(stmt1, thisConnection);
+
+            thisConnection.Open();
+
+            SqlDataReader dr = cmdSelect.ExecuteReader();
+
+            if (dr.HasRows)
+            {
+                try
+                {
+                    dr.Read();
+                    ID = (int)dr[0] + 1;
+                }
+                catch (Exception ex)
+                {
+                    ID = 0;
+                }
+            }
+            else
+            {
+                ID = 0;
+            }
+
+            String stmt2 = "SELECT ID from Messages where ID = " + ID.ToString();
+
+            mes.Create(s, d, r, n);
+
+            SqlCommand cmdSelect2 = new SqlCommand(stmt2, thisConnection);
+
+            dr.Close();
+
+            SqlDataReader dr2 = cmdSelect.ExecuteReader();
+
+            if (dr2.HasRows)
+            {
+                Label1.Text = "Message has been sent.";
+                Label1.Visible = true;
+            }
+            else
+            {
+                Label1.Text = "Message has not been sent.";
+                Label1.Visible = true;
+            }
+
+            dr.Close();
+        }
+
+    }
+}
