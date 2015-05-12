6e3146eff74c4c1e8f660016b13efbd3ac48e8b0
Jorge Vergara <jorgevergara1993@gmail.com>
2015-05-12 22:16:48 +0200

    SearchbyName de los events done

--- ProjectGen/ProjectGenNHibernate/CEN/Project/EventsCEN_searchByName.cs
+++ ProjectGen/ProjectGenNHibernate/CEN/Project/EventsCEN_searchByName.cs
@@ -19,8 +19,32 @@ public System.Collections.Generic.IList<ProjectGenNHibernate.EN.Project.EventsEN
         /*PROTECTED REGION ID(ProjectGenNHibernate.CEN.Project_Events_searchByName) ENABLED START*/
 
         // Write here your custom code...
+    System.Collections.Generic.IList<ProjectGenNHibernate.EN.Project.EventsEN> lista = new System.Collections.Generic.List<ProjectGenNHibernate.EN.Project.EventsEN>();
+    System.Collections.Generic.IList<ProjectGenNHibernate.EN.Project.EventsEN> ret = new System.Collections.Generic.List<ProjectGenNHibernate.EN.Project.EventsEN>();
+    ProjectGenNHibernate.CEN.Project.EventsCEN evencen = new EventsCEN();
 
-        throw new NotImplementedException ("Method SearchByName() not yet implemented.");
+    try
+    {
+        SessionInitializeTransaction();
+        EventsCAD eve = new EventsCAD(session);
+        EventsCAD evecad = new EventsCAD(session);
+        lista = eve.GetAll();
+
+        foreach (EventsEN h in lista)
+        {
+            if(h.Name.Contains(arg1)){
+                ret.Add(h);
+            }
+        }
+        SessionCommit();
+    }
+    catch (Exception ex)
+    {
+        SessionRollBack();
+    }
+
+    return ret;
+        //throw new NotImplementedException ("Method SearchByName() not yet implemented.");
 
         /*PROTECTED REGION END*/
 }
