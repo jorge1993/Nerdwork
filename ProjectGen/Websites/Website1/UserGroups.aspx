54a3c4a9e9538597bfca3158ddaa7f90f5dcb609
trokotrokos <trokotrokos@hotmail.com>
2015-05-15 15:09:47 +0200

    Gridview de grupos (a falta de searchbyname)

--- ProjectGen/Websites/Website1/UserGroups.aspx
+++ ProjectGen/Websites/Website1/UserGroups.aspx
@@ -5,5 +5,8 @@
 </asp:Content>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <div>espacio</div>
+    <div><asp:GridView ID="GridView1" runat="server">
+    </asp:GridView></div>
 </asp:Content>
 
--- ProjectGen/Websites/Website1/UserGroups.aspx.cs
+++ ProjectGen/Websites/Website1/UserGroups.aspx.cs
@@ -4,11 +4,66 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ProjectGenNHibernate.CEN.Project;
+using ProjectGenNHibernate.EN.Project;
+using System.Data;
 
 public partial class UserGroups : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        reloadTimeLine();
+    }
+
+    private void reloadTimeLine()
+    {
+
+        IList<GroupsEN> dr = new List<GroupsEN>();
+        GroupsCEN eve = new GroupsCEN();
+
+        dr = eve.GetAllGroups();
+        int size = eve.GetAllGroups().Count;
+
+        DataTable dt = new DataTable();
+        dt.Columns.Add("name", typeof(string));
+        dt.Columns.Add("description", typeof(string));
+        dt.Columns.Add("hobbies", typeof(string));
+
+
+        for (int j = 0; j < size; j++)
+        {
+
+            DataRow Row1;
+            string listaHobbies = "";
+            Row1 = dt.NewRow();
+            IList<GroupsEN> use = new List<GroupsEN>();
+
+            use = eve.SearchByName(dr[j].Name);
+
+            Row1[0] = use[0].Name;
+            Row1[1] = use[0].Description;
+            
+
+            IList<HobbyEN> listaHobby = new List<HobbyEN>();
+            HobbyCEN hobbycen = new HobbyCEN();
+            listaHobby = hobbycen.GetHobbybyID(dr[j].Id);
+            int aux = listaHobby.Count;
+            int contador = 1;
+
+            foreach (HobbyEN hobby in listaHobby)
+            {
+                listaHobbies += hobby.Name;
+                if (aux != contador)
+                    listaHobbies += " - ";
+                contador++;
+            }
+
+            Row1[3] = listaHobbies;
+
+            dt.Rows.Add(Row1);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
 
     }
 }
