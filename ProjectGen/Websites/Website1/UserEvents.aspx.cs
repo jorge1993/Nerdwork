60c90e3b9369d8a76ad932083a51c934742947cd
CARLOS MARTINEZ RUEDA <cmr66@alu.ua.es>
2015-05-15 12:09:39 +0200

    asd

--- ProjectGen/Websites/Website1/UserEvents.aspx.cs
+++ ProjectGen/Websites/Website1/UserEvents.aspx.cs
@@ -4,12 +4,68 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ProjectGenNHibernate.CEN.Project;
+using ProjectGenNHibernate.EN.Project;
+using System.Data;
 
 public partial class UserEvents : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        reloadTimeLine();
+    }
 
+    private void reloadTimeLine()
+    {
+        
+            IList<EventsEN> dr = new List<EventsEN>();
+            EventsCEN eve = new EventsCEN();
+
+            dr = eve.GetAll();
+            int size = eve.GetAll().Count;
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("name", typeof(string));
+            dt.Columns.Add("description", typeof(string));
+            dt.Columns.Add("place", typeof(string));
+            dt.Columns.Add("hobbies", typeof(string));
+
+
+            for (int j = 0; j < size; j++)
+            {
+
+                DataRow Row1;
+                string listaHobbies = "";
+                Row1 = dt.NewRow();
+                IList<EventsEN> use = new List<EventsEN>();
+
+                use = eve.SearchByName(dr[j].Name);
+
+                Row1[0] = use[0].Name;
+                Row1[1] = use[0].Description;
+                Row1[2] = use[0].Place;
+
+                IList<HobbyEN> listaHobby = new List<HobbyEN>();
+                HobbyCEN hobbycen = new HobbyCEN();
+                listaHobby = hobbycen.GetHobbybyID(dr[j].Id);
+                int aux = listaHobby.Count;
+                int contador = 1;
+
+                foreach (HobbyEN hobby in listaHobby)
+                {
+                    listaHobbies += hobby.Name;
+                    if (aux != contador)
+                        listaHobbies += " - ";
+                    contador++;
+                }
+
+                Row1[3] = listaHobbies;
+
+                dt.Rows.Add(Row1);
+                GridViewTimeline.DataSource = dt;
+                GridViewTimeline.DataBind();
+            }
+          
     }
 
 
