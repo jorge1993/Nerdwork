ce631bbeb248e03f16288da786a8b4df2e9dc215
Jorge Vergara <jorgevergara1993@gmail.com>
2015-05-16 20:21:27 +0200

    getAllHobbies de Eventos hecho

--- ProjectGen/ProjectGenNHibernate/CEN/Project/EventosCEN_getAllHobbies.cs
+++ ProjectGen/ProjectGenNHibernate/CEN/Project/EventosCEN_getAllHobbies.cs
@@ -12,15 +12,36 @@ using ProjectGenNHibernate.CAD.Project;
 
 namespace ProjectGenNHibernate.CEN.Project
 {
-public partial class EventosCEN
+public partial class EventosCEN : BasicCAD
 {
 public System.Collections.Generic.IList<ProjectGenNHibernate.EN.Project.HobbyEN> GetAllHobbies (int arg0)
 {
         /*PROTECTED REGION ID(ProjectGenNHibernate.CEN.Project_Eventos_getAllHobbies) ENABLED START*/
 
         // Write here your custom code...
+    System.Collections.Generic.IList<ProjectGenNHibernate.EN.Project.HobbyEN> aux = new System.Collections.Generic.List<HobbyEN>();
+    System.Collections.Generic.IList<ProjectGenNHibernate.EN.Project.HobbyEN> lista = new System.Collections.Generic.List<HobbyEN>();
+    try
+    {
+        SessionInitializeTransaction();
+        EventosCAD eve = new EventosCAD(session);
+        HobbyCAD hobbycad = new HobbyCAD(session);
+        EventosEN even = eve.ReadOIDDefault(arg0);
+        aux = hobbycad.GetAllHobby();
 
-        throw new NotImplementedException ("Method GetAllHobbies() not yet implemented.");
+        foreach (HobbyEN h in aux)
+        {
+            if (h.Name.Equals(even.Hobby))
+                lista.Add(h);
+        }
+        SessionCommit();
+    }
+    catch (Exception ex)
+    {
+        SessionRollBack();
+    }
+
+    return lista;
 
         /*PROTECTED REGION END*/
 }
